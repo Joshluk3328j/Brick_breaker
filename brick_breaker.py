1ad041f1be5ea402ac06d36e70e48ba6139282b9
Joshluk3328j <babatundejoseph24810@gmail.com>
2022-11-10 16:55:14 +0100

    i made the board stop at the boundary

--- brick_breaker.py
+++ brick_breaker.py
@@ -3,22 +3,14 @@ from random import random
 import turtle
 import time
 import random
+import pygame
+from pygame import event, K_a, K_LEFT, QUIT, key, KEYDOWN
 
 # screen
 wn = turtle.Screen()
 wn.screensize(500, 500)
 wn.bgcolor('black')
 wn.title('Brick Breaker')
-
-# pen
-pen = turtle.Turtle()
-pen.speed(0)
-pen.color('green')
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 230)
-pen.write('Score: 0  High score: 0', align='center', font=("Helvetica", "16"))
-
 
 # boundary
 pen2 = turtle.Turtle()
@@ -40,14 +32,16 @@ def drw_boundary():
 
 
 drw_boundary()
-# ball
-ball = turtle.Turtle()
-ball.speed(3)
-ball.shape("circle")
-ball.color("red")
-ball.penup()
-ball.goto(0, -230)
-ball.direction = "stop"
+
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('green')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 230)
+pen.write('Score: 0  High score: 0', align='center', font=("Helvetica", "16"))
+
 
 # board
 board = turtle.Turtle()
@@ -56,10 +50,19 @@ board.shape("square")
 board.color("purple")
 board.shapesize(1, 5)
 board.penup()
-b_y = -250
+b_y = -270
 board.goto(0, b_y)
 board.direction = "stop"
 
+
+# ball
+ball = turtle.Turtle()
+ball.speed(3)
+ball.shape("circle")
+ball.color("red")
+ball.penup()
+ball.goto(0, b_y)
+ball.direction = "stop"
 
 # make bricks
 # def make_bricks():
@@ -114,19 +117,17 @@ board.direction = "stop"
 
 def move_right():
     x = board.xcor()
-    x += 40
-    board.goto(x, b_y)
+    x += 30
+    if board.xcor() > -250 and board.xcor() < 250:
+        board.goto(x, b_y)
 
 
 def move_left():
     x = board.xcor()
-    x -= 40
-    board.goto(x, b_y)
+    x -= 30
+    if board.xcor() > -240 and board.xcor() < 310:
+        board.goto(x, b_y)
 
-
-wn.listen()
-wn.onkeypress(move_right, "d")
-wn.onkeypress(move_left, "a")
 
 angle = random.randint(30, 90)
 ball.left(angle)
@@ -144,11 +145,10 @@ while True:
         # this represents the boundary
         wn.update()
         ball.forward(5)
-        # if ball.distance(brick) < 20:
-        #     ball.left(angle)
-        #     ball.forward(7)
-
-        # the ball will now stop at the boundary
+        # or -20 < ball.xcor()-board.xcor() < 20:
+        if board.distance(ball) < 20:  # -5 < ball.ycor()-board.ycor() < 20:
+            ball.left(angle)
+            ball.forward(100)
     elif ball.ycor() <= -280:
         pen.goto(0, 0)
         pen.write('Game Over', align='center', font=("Helvetica", "16"))
@@ -163,14 +163,12 @@ while True:
         ball.left(angle)
         ball.forward(7)
     else:
+        # this makes the ball bounce at the boundary
         ball.left(angle)
         ball.forward(7)
-        while ball.xcor() > -280 and ball.xcor() < 310 and ball.ycor() > -280 and ball.ycor() < 200:  # this represents the boundary
+        while ball.xcor() > -280 and ball.xcor() < 310 and ball.ycor() > -280 and ball.ycor() < 200:
             wn.update()
             ball.forward(7)
-    if b_y - ball_y < 5 and -5 < b_x - ball_x < 5:
-        ball.left(angle)
-        ball.forward(7)
 
 wn.mainloop()
 
