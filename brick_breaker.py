67c635664121ad39c0e80178b13c7cb40c75dcc9
Joshluk3328j <babatundejoseph24810@gmail.com>
2022-11-12 17:59:14 +0100

    added scores

--- brick_breaker.py
+++ brick_breaker.py
@@ -3,8 +3,6 @@ from random import random
 import turtle
 import time
 import random
-import pygame
-from pygame import event, K_a, K_LEFT, QUIT, key, KEYDOWN
 
 # screen
 wn = turtle.Screen()
@@ -16,6 +14,8 @@ wn.title('Brick Breaker')
 pen2 = turtle.Turtle()
 pen2.color('white')
 pen2.penup()
+
+segments = []
 
 
 def drw_boundary():
@@ -32,6 +32,8 @@ def drw_boundary():
 
 
 drw_boundary()
+score = 0
+# High_score = 0
 
 # pen
 pen = turtle.Turtle()
@@ -46,14 +48,65 @@ pen.write('Score: 0  High score: 0', align='center', font=("Helvetica", "16"))
 # board
 board = turtle.Turtle()
 board.speed(0)
-board.shape("square")
-board.color("purple")
-board.shapesize(1, 5)
 board.penup()
 b_y = -270
 board.goto(0, b_y)
-board.direction = "stop"
+wn.addshape('board.gif')
+board.shape('board.gif')
 
+
+def make_bricks():
+    x = 300
+    y = 190
+    for brick in range(20):
+        brick = turtle.Turtle()
+        brick.speed(0)
+        brick.color('black')
+        brick.shape('square')
+        brick.penup()
+        brick.hideturtle()
+        brick.goto(x, y)
+        brick.color('blue')
+        brick.showturtle()
+        segments.append(brick)
+        x -= 30
+        if x == -300:
+            break
+    x = 300
+    y = 140
+    for brick in range(20):
+        brick = turtle.Turtle()
+        brick.speed(0)
+        brick.color('black')
+        brick.shape('square')
+        brick.penup()
+        brick.hideturtle()
+        brick.goto(x, y)
+        brick.color('blue')
+        brick.showturtle()
+        segments.append(brick)
+        x -= 30
+        if x == -300:
+            break
+    x = 300
+    y = 90
+    for brick in range(20):
+        brick = turtle.Turtle()
+        brick.speed(0)
+        brick.color('black')
+        brick.shape('square')
+        brick.penup()
+        brick.hideturtle()
+        brick.goto(x, y)
+        brick.color('blue')
+        brick.showturtle()
+        segments.append(brick)
+        x -= 30
+        if x == -300:
+            break
+
+
+make_bricks()
 
 # ball
 ball = turtle.Turtle()
@@ -61,71 +114,21 @@ ball.speed(3)
 ball.shape("circle")
 ball.color("red")
 ball.penup()
-ball.goto(0, b_y)
+ball.goto(board.xcor(), board.ycor()+20)
 ball.direction = "stop"
 
-# make bricks
-# def make_bricks():
-# x = 300
-# y = 190
-# for brick in range(20):
-#     brick = turtle.Turtle()
-#     brick.speed(0)
-#     brick.color('black')
-#     brick.shape('square')
-#     brick.penup()
-#     brick.hideturtle()
-#     brick.goto(x, y)
-#     brick.color('blue')
-#     brick.showturtle()
-#     x -= 30
-#     if x == -300:
-#         break
-# x = 300
-# y = 140
-# for brick in range(20):
-#     brick = turtle.Turtle()
-#     brick.speed(0)
-#     brick.color('black')
-#     brick.shape('square')
-#     brick.penup()
-#     brick.hideturtle()
-#     brick.goto(x, y)
-#     brick.color('blue')
-#     brick.showturtle()
-#     x -= 30
-#     if x == -300:
-#         break
-# x = 300
-# y = 90
-# for brick in range(20):
-#     brick = turtle.Turtle()
-#     brick.speed(0)
-#     brick.color('black')
-#     brick.shape('square')
-#     brick.penup()
-#     brick.hideturtle()
-#     brick.goto(x, y)
-#     brick.color('blue')
-#     brick.showturtle()
-#     x -= 30
-#     if x == -300:
-#         break
-
-
-# make_bricks()
 
 def move_right():
     x = board.xcor()
-    x += 30
-    if board.xcor() > -250 and board.xcor() < 250:
+    x += 25
+    if board.xcor() > -290 and board.xcor() < 265:
         board.goto(x, b_y)
 
 
 def move_left():
     x = board.xcor()
-    x -= 30
-    if board.xcor() > -240 and board.xcor() < 310:
+    x -= 25
+    if board.xcor() > -241 and board.xcor() < 310:
         board.goto(x, b_y)
 
 
@@ -141,15 +144,24 @@ while True:
     b_x = board.xcor()
     ball_y = ball.ycor()
     ball_x = ball.xcor()
-    if ball.xcor() > -280 and ball.xcor() < 310 and ball.ycor() < 200:
+    while ball.xcor() > -280 and ball.xcor() < 310 and ball.ycor() < 200:
         # this represents the boundary
         wn.update()
         ball.forward(5)
-        # or -20 < ball.xcor()-board.xcor() < 20:
-        if board.distance(ball) < 20:  # -5 < ball.ycor()-board.ycor() < 20:
+        if ball.distance(board) < 20:
             ball.left(angle)
             ball.forward(100)
-    elif ball.ycor() <= -280:
+        else:
+            for i in segments:
+                if ball.distance(i) < 20:
+                    i.hideturtle()
+                    ball.left(angle)
+                    ball.forward(20)
+                    score += 20
+                    pen.clear()
+                    pen.write(f'Score: {score}  High score: 0',
+                              align='center', font=("Helvetica", "16"))
+    if ball.ycor() <= -280:
         pen.goto(0, 0)
         pen.write('Game Over', align='center', font=("Helvetica", "16"))
         ball.hideturtle
@@ -157,6 +169,7 @@ while True:
         pen.clear()
         ball.goto(board.xcor(), board.ycor()+20)
         pen.goto(0, 230)
+        score = 0
         pen.write('Score: 0  High score: 0',
                   align='center', font=("Helvetica", "16"))
         ball.showturtle
